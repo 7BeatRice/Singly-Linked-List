d7b4cf3befe8a6cd7dd5062cfbd08073eddefd8d
Beatrice Olaosebikan <beat1rice2ola@gmail.com>
2025-03-16 21:38:11 -0500

    initial commit

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+import java.util.List;
+public class Node {
+    String stateName;
+    List<String> neighbors;
+    Node next;
+
+    // Constructor to initialize the node
+    public Node(String stateName, List<String> neighbors) {
+        this.stateName = stateName;
+        this.neighbors = neighbors;
+    }
+}
